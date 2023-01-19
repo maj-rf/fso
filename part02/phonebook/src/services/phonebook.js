7c9a698579c624073f2217edd718a47d1df48696
bananabread08 <rmkfermin@gmail.com>
2023-01-19 11:22:43 +0800

    finish 3.9-3.11

--- part02/phonebook/src/services/phonebook.js
+++ part02/phonebook/src/services/phonebook.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/persons';
+const baseUrl = '/api/persons';
 
 export const getAllContacts = async () => {
   const response = await axios.get(baseUrl);
