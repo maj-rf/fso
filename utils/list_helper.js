7236a6c9e94e278788857c8baf83b2ea7729cc84
bananabread08 <rmkfermin@gmail.com>
2023-01-23 11:01:04 +0800

    finish 4.5-4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -11,7 +11,45 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const reducer = (acc, current) => {
+    return acc.likes > current.likes ? acc : current;
+  };
+  return blogs.length === 0 ? null : blogs.reduce(reducer, blogs[0]);
+};
+
+const getMaxofMap = (blogs, type) => {
+  const artistMap = new Map();
+  for (const blog of blogs) {
+    const author = blog.author;
+    const value = type === 'likes' ? blog.likes : 1;
+    if (artistMap.has(author))
+      artistMap.set(author, artistMap.get(author) + value);
+    else artistMap.set(author, value);
+  }
+  // get the largest count using reduce. convert map to array
+  const max = [...artistMap.entries()].reduce((acc, curr) => {
+    return curr[1] > acc[1] ? curr : acc;
+  });
+  return max;
+};
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null;
+  const max = getMaxofMap(blogs, 'blogs');
+  return { author: max[0], blogs: max[1] };
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return null;
+  const max = getMaxofMap(blogs, 'likes');
+  return { author: max[0], likes: max[1] };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
