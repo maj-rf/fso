1a6ef79485ee305aab08827eac402eb15366ec70
bananabread08 <rmkfermin@gmail.com>
2023-01-25 10:42:57 +0800

    refactor mongoose connection and change env vars when in test

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 
-let PORT = process.env.PORT;
-let MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT;
+
+const MONGO_URI =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGO_URI
+    : process.env.MONGO_URI;
 
 module.exports = {
   MONGO_URI,
