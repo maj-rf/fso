2771e9ef6a722ec90a940e378a24dc33000f1359
bananabread08 <rmkfermin@gmail.com>
2023-01-26 11:36:25 +0800

    update 4.8-4.12 with express-async-errors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const config = require('./utils/config');
 const express = require('express');
+require('express-async-errors');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const blogRouter = require('./controllers/blogController');
