0cfb24a0e33eef3afb6e81c795cadc8ae38f239e
bananabread08 <rmkfermin@gmail.com>
2023-01-27 11:00:18 +0800

    create user model and update blog model

--- models/blog.js
+++ models/blog.js
@@ -5,6 +5,10 @@ const blogSchema = new mongoose.Schema({
   author: { type: String, required: true },
   url: String,
   likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 });
 
 blogSchema.set('toJSON', {
