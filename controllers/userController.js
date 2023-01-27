606a88f3752f7065b5205ff28e99e6f61d6da47f
bananabread08 <rmkfermin@gmail.com>
2023-01-27 11:01:40 +0800

    create userController and update blogController

---
+++ controllers/userController.js
@@ -0,0 +1,29 @@
+const bcrypt = require('bcrypt');
+const userRouter = require('express').Router();
+const User = require('../models/user');
+
+userRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body;
+  if (!username || !name || !password) {
+    return response.status(400).end();
+  }
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+  response.status(201).json(savedUser);
+});
+
+userRouter.get('/', async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
+});
+
+module.exports = userRouter;
