f793d74c40b6ae0410cecf8d7d49574ce213ea8f
bananabread08 <rmkfermin@gmail.com>
2023-02-01 11:05:20 +0800

    create tokenExtractor middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -24,8 +24,17 @@ const errorHandler = (error, request, response, next) => {
   next(error);
 };
 
+const tokenExtractor = (request, response, next) => {
+  const auth = request.get('authorization');
+  if (auth && auth.startsWith('Bearer ')) {
+    request.token = auth.replace('Bearer ', '');
+  }
+  next();
+};
+
 module.exports = {
   unknownEndpoint,
   errorHandler,
   morgan,
+  tokenExtractor,
 };
