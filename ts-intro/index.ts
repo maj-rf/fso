cc90068b1e631ca006bb2813e90dfbbfa47fdac7
bananabread08 <rmkfermin@gmail.com>
2023-03-03 09:41:52 +0800

    create express entrypoint

---
+++ ts-intro/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('pong');
+});
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
