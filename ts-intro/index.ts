5d913c334edff8e1f574003be2287d1dcd3a7e52
bananabread08 <rmkfermin@gmail.com>
2023-03-03 09:46:04 +0800

    finish 9.4. create hello endpoint

--- ts-intro/index.ts
+++ ts-intro/index.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('pong');
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack');
 });
 
 const PORT = 3003;
