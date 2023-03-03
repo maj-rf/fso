ff466e9f2a9b19a21ee714a453668a120cbdb5b6
bananabread08 <rmkfermin@gmail.com>
2023-03-03 09:56:49 +0800

    finish 9.5. create bmi endpoints with URL queries

--- ts-intro/bmiCalculator.ts
+++ ts-intro/bmiCalculator.ts
@@ -1,4 +1,4 @@
-function calculateBMI(height: number, weight: number) {
+export function calculateBMI(height: number, weight: number) {
   const bmi = weight / (((height / 100) * height) / 100);
   if (bmi < 18.5) return 'Underweight';
   else if (bmi > 24.9) return 'Overweight';
--- ts-intro/index.ts
+++ ts-intro/index.ts
@@ -1,8 +1,28 @@
 import express from 'express';
+import { calculateBMI } from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack');
+});
+
+app.get('/bmi', (req, res) => {
+  if (!req.query.height || !req.query.weight) {
+    res
+      .status(400)
+      .json({
+        error: 'Invalid Parameters: Please provide both height and weight',
+      });
+  }
+  if (isNaN(Number(req.query.height)) || isNaN(Number(req.query.weight))) {
+    res.status(400).json({
+      error:
+        'Malformatted Parameters: Height and weight should both be numbers',
+    });
+  }
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+  res.send({ height, weight, bmi: calculateBMI(height, weight) });
 });
 
 const PORT = 3003;
