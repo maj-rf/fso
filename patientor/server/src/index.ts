d90b26ed5f100d3bcdf015a2066fb7ab0e30a605
bananabread08 <rmkfermin@gmail.com>
2023-03-06 09:45:44 +0800

    use cors and routers

--- patientor/server/src/index.ts
+++ patientor/server/src/index.ts
@@ -1,12 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+
+import diagnosesRouter from './router/diagnosesRouter';
+import patientsRouter from './router/diagnosesRouter';
 const app = express();
 app.use(express.json());
-const PORT = 3000;
+app.use(cors());
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone ponged');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
+app.use('/api/patients', patientsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
