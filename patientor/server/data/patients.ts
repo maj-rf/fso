eb2d224f784b0fe36c46ebd092a7a3d185d6a2ed
bananabread08 <rmkfermin@gmail.com>
2023-03-07 10:55:47 +0800

    parse data to satisfy Gender enums

--- patientor/server/data/patients.ts
+++ patientor/server/data/patients.ts
@@ -1,6 +1,6 @@
 import { Patient } from '../src/types';
-
-export const data: Patient[] = [
+import { toNewPatientEntry } from '../src/utils/toNewPatientEntry';
+const unParsedData = [
   {
     id: 'd2773336-f723-11e9-8f0b-362b9e155667',
     name: 'John McClane',
@@ -42,3 +42,9 @@ export const data: Patient[] = [
     occupation: 'Digital evangelist',
   },
 ];
+
+export const data: Patient[] = unParsedData.map((obj) => {
+  const object = toNewPatientEntry(obj) as Patient;
+  object.id = obj.id;
+  return object;
+});
