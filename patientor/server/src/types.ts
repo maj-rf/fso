e24fef336256064c9f6e7cb2d52d54a42e60aa19
bananabread08 <rmkfermin@gmail.com>
2023-03-07 10:54:35 +0800

    create enums for Gender

--- patientor/server/src/types.ts
+++ patientor/server/src/types.ts
@@ -1,3 +1,9 @@
+export enum Gender {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other',
+}
+
 export interface Diagnose {
   code: string;
   name: string;
@@ -9,7 +15,7 @@ export interface Patient {
   name: string;
   dateOfBirth: string;
   ssn: string;
-  gender: string;
+  gender: Gender;
   occupation: string;
 }
 
