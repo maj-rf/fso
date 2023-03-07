04d4b131caa935498275020f453ff579c13302bc
bananabread08 <rmkfermin@gmail.com>
2023-03-07 09:09:24 +0800

    finish 9.12

--- patientor/server/src/router/patientsRouter.ts
+++ patientor/server/src/router/patientsRouter.ts
@@ -1,10 +1,24 @@
 import express from 'express';
 import { getPatients } from '../services/patientsServices';
-
+import { addNewPatient } from '../services/patientsServices';
+import { NewPatient } from '../types';
 const patientsRouter = express.Router();
 
 patientsRouter.get('/', (_req, res) => {
   const data = getPatients();
   res.json(data);
 });
+
+patientsRouter.post('/', (req, res) => {
+  const { name, ssn, dateOfBirth, occupation, gender } = req.body as NewPatient;
+  const addedPatient = addNewPatient({
+    name,
+    ssn,
+    dateOfBirth,
+    occupation,
+    gender,
+  });
+  res.json(addedPatient);
+});
+
 export default patientsRouter;
