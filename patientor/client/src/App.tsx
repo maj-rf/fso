a2b5fc019f45bab0096260e189db77f91c896209
bananabread08 <rmkfermin@gmail.com>
2023-03-20 15:40:24 +0800

    finish 9.23-9.24
    add diagnosis state and pass props to patient component

--- patientor/client/src/App.tsx
+++ patientor/client/src/App.tsx
@@ -4,14 +4,16 @@ import { Route, Link, Routes } from 'react-router-dom';
 import { Button, Divider, Container, Typography } from '@mui/material';
 
 import { apiBaseUrl } from './constants';
-import { Patient } from './types';
+import { Diagnose, Patient } from './types';
 import { PatientComponent } from './components/PatientListPage/PatientComponent';
 
 import patientService from './services/patients';
 import PatientListPage from './components/PatientListPage';
+import { getAllDiagnoses } from './services/diagnosis';
 
 const App = () => {
   const [patients, setPatients] = useState<Patient[]>([]);
+  const [diagnosis, setDiagnosis] = useState<Diagnose[]>([]);
   useEffect(() => {
     void axios.get<void>(`${apiBaseUrl}/ping`);
 
@@ -20,6 +22,12 @@ const App = () => {
       setPatients(patients);
     };
     void fetchPatientList();
+
+    const fetchDiagnosis = async () => {
+      const data = await getAllDiagnoses();
+      setDiagnosis(data);
+    };
+    void fetchDiagnosis();
   }, []);
 
   return (
@@ -39,7 +47,10 @@ const App = () => {
               <PatientListPage patients={patients} setPatients={setPatients} />
             }
           />
-          <Route path="/:id" element={<PatientComponent />} />
+          <Route
+            path="/:id"
+            element={<PatientComponent diagnosis={diagnosis} />}
+          />
         </Routes>
       </Container>
     </div>
--- patientor/client/src/components/PatientListPage/PatientComponent.tsx
+++ patientor/client/src/components/PatientListPage/PatientComponent.tsx
@@ -1,10 +1,17 @@
-import { Card, CardHeader, List, ListItem } from '@mui/material';
+import {
+  Card,
+  CardHeader,
+  List,
+  ListItem,
+  CardContent,
+  Typography,
+} from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getPatient } from '../../services/patients';
-import { Patient } from '../../types';
+import { Diagnose, Patient } from '../../types';
 
-export const PatientComponent = () => {
+export const PatientComponent = ({ diagnosis }: { diagnosis: Diagnose[] }) => {
   const [patient, setPatient] = useState<Patient | null>(null);
   const { id } = useParams();
 
@@ -20,10 +27,30 @@ export const PatientComponent = () => {
   return (
     <Card sx={{ maxWidth: 'max-content' }}>
       <CardHeader title={patient?.name} />
-      <List>
-        <ListItem>Occupation: {patient?.occupation}</ListItem>
-        <ListItem>SSN: {patient?.ssn}</ListItem>
-      </List>
+      <CardContent>
+        <Typography variant="body1" component="div">
+          Occupation: {patient?.occupation}
+        </Typography>
+        <Typography gutterBottom variant="body1" component="div">
+          SSN: {patient?.ssn}
+        </Typography>
+        <Typography variant="h6">Entries</Typography>
+        {patient?.entries.map((entry) => {
+          return (
+            <List key={entry.id}>
+              <ListItem>{entry.date}</ListItem>
+              <ListItem>{entry.description}</ListItem>
+              <ul>
+                {entry.diagnosisCodes?.map((code) => (
+                  <li key={code}>
+                    {code}: {diagnosis.find((d) => d.code === code)?.name}
+                  </li>
+                ))}
+              </ul>
+            </List>
+          );
+        })}
+      </CardContent>
     </Card>
   );
 };
