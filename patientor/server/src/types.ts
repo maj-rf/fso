da4a1d49fe5be4b094f0abca3bc515dbfa8ab815
bananabread08 <rmkfermin@gmail.com>
2023-03-20 14:48:58 +0800

    add types and union types for entry and entry types

--- patientor/server/src/types.ts
+++ patientor/server/src/types.ts
@@ -10,7 +10,47 @@ export interface Diagnose {
   latin?: string;
 }
 
-export interface Entry {}
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: { startDate: string; endDate: string };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: { date: string; criteria: string };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+export const EntryTypes = [
+  'HealthCheck',
+  'Hospital',
+  'OccupationalHealthcare',
+] as const;
 
 export interface Patient {
   id: string;
