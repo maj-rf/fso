b8a8197ab75a7b810bbf4690ba23c625a443a3d7
bananabread08 <rmkfermin@gmail.com>
2023-03-21 13:03:55 +0800

    partial completion of 9.27

--- patientor/client/src/components/PatientListPage/PatientComponent.tsx
+++ patientor/client/src/components/PatientListPage/PatientComponent.tsx
@@ -1,12 +1,23 @@
-import { Typography, Container, Box } from '@mui/material';
+import { Typography, Container, Box, Button } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getPatient } from '../../services/patients';
 import { Diagnose, Patient } from '../../types';
+import { AddEntryModal } from '../AddEntryModal';
 import { EntryDetails } from '../EntryDetails/EntryDetails';
 
 export const PatientComponent = ({ diagnosis }: { diagnosis: Diagnose[] }) => {
   const [patient, setPatient] = useState<Patient | null>(null);
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
+
+  const openModal = (): void => setModalOpen(true);
+
+  const closeModal = (): void => {
+    setModalOpen(false);
+    setError(undefined);
+  };
+
   const { id } = useParams();
 
   useEffect(() => {
@@ -21,6 +32,9 @@ export const PatientComponent = ({ diagnosis }: { diagnosis: Diagnose[] }) => {
   return (
     <Container maxWidth="sm">
       <Box sx={{ bgcolor: '#cfe8fc', p: 2 }}>
+        <Button variant="contained" onClick={() => openModal()}>
+          Add New Entry
+        </Button>
         <Typography variant="h4">{patient?.name}</Typography>
         <Typography variant="body1" component="div">
           Occupation: {patient?.occupation}
@@ -35,6 +49,12 @@ export const PatientComponent = ({ diagnosis }: { diagnosis: Diagnose[] }) => {
           );
         })}
       </Box>
+      <AddEntryModal
+        modalOpen={modalOpen}
+        onSubmit={() => console.log('open')}
+        error={error}
+        onClose={closeModal}
+      />
     </Container>
   );
 };
