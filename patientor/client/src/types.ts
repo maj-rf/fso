545964d7fe005043dfc63181a4bddf52dd073f5b
bananabread08 <rmkfermin@gmail.com>
2023-03-21 10:54:42 +0800

    export Entry types

--- patientor/client/src/types.ts
+++ patientor/client/src/types.ts
@@ -25,18 +25,18 @@ export enum HealthCheckRating {
   'CriticalRisk' = 3,
 }
 
-interface HealthCheckEntry extends BaseEntry {
+export interface HealthCheckEntry extends BaseEntry {
   type: 'HealthCheck';
   healthCheckRating: HealthCheckRating;
 }
 
-interface OccupationalHealthcareEntry extends BaseEntry {
+export interface OccupationalHealthcareEntry extends BaseEntry {
   type: 'OccupationalHealthcare';
   employerName: string;
   sickLeave?: { startDate: string; endDate: string };
 }
 
-interface HospitalEntry extends BaseEntry {
+export interface HospitalEntry extends BaseEntry {
   type: 'Hospital';
   discharge: { date: string; criteria: string };
 }
