612ef59e036f8b3a62433f598279d96ffc0a8b41
bananabread08 <rmkfermin@gmail.com>
2023-03-21 13:00:37 +0800

    create post endpoint for patients/id

--- patientor/server/src/services/patientsServices.ts
+++ patientor/server/src/services/patientsServices.ts
@@ -1,5 +1,11 @@
+import { Entry } from './../../../client/src/types';
 import { data } from '../../data/patients';
-import { Patient, NonSensitivePatientData, NewPatient } from '../types';
+import {
+  Patient,
+  NonSensitivePatientData,
+  NewPatient,
+  EntryWithoutId,
+} from '../types';
 import { v1 as uuid } from 'uuid';
 
 export const getPatients = (): Patient[] => {
@@ -24,5 +30,19 @@ export const addNewPatient = (entry: NewPatient): Patient => {
     ...entry,
   };
   data.push(newPatientEntry);
+  return newPatientEntry;
+};
+
+export const addNewEntry = (
+  currentID: string,
+  entry: EntryWithoutId
+): Entry => {
+  const id = uuid();
+  const currentPatient = data.find((x) => x.id === currentID);
+  if (!currentPatient) {
+    throw new Error(`No matching patient ID found for: ${currentID}`);
+  }
+  const newPatientEntry = { id, ...entry };
+  currentPatient?.entries.push(newPatientEntry);
   return newPatientEntry;
 };
