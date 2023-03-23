4ed109ed2556d312f5fc5cf193fa18baa0f42b33
bananabread08 <rmkfermin@gmail.com>
2023-03-23 11:37:02 +0800

    finish 6.3-6.4
    create vote and add anecdote functionality in reducers

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,34 +1,55 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux';
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const sorted = anecdotes.sort((a, b) => b.votes - a.votes);
+  const voteThis = (id) => {
+    return {
+      type: 'VOTE',
+      payload: { id },
+    };
+  };
+
+  const createThis = (content) => {
+    return {
+      type: 'CREATE',
+      payload: { content },
+    };
+  };
 
   const vote = (id) => {
-    console.log('vote', id)
-  }
+    dispatch(voteThis(id));
+  };
+
+  const createNewAnecdote = (e) => {
+    e.preventDefault();
+    const content = e.target.anecdote.value;
+    e.target.anecdote.value = '';
+    dispatch(createThis(content));
+  };
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {sorted.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => vote(anecdote.id)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
+      <form onSubmit={createNewAnecdote}>
+        <div>
+          <input name="anecdote" />
+        </div>
         <button>create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -4,26 +4,40 @@ const anecdotesAtStart = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+];
 
-const getId = () => (100000 * Math.random()).toFixed(0)
+const getId = () => (100000 * Math.random()).toFixed(0);
 
 const asObject = (anecdote) => {
   return {
     content: anecdote,
     id: getId(),
-    votes: 0
-  }
-}
+    votes: 0,
+  };
+};
 
-const initialState = anecdotesAtStart.map(asObject)
+const initialState = anecdotesAtStart.map(asObject);
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  switch (action.type) {
+    case 'VOTE':
+      const id = action.payload.id.toString();
+      const voted = state.find((a) => a.id === id);
+      return state.map((a) =>
+        a.id === id
+          ? {
+              ...voted,
+              votes: voted.votes + 1,
+            }
+          : a
+      );
+    case 'CREATE':
+      const newAnecdote = asObject(action.payload.content);
+      return state.concat(newAnecdote);
+    default:
+      return state;
+  }
+};
 
-  return state
-}
-
-export default reducer
+export default reducer;
