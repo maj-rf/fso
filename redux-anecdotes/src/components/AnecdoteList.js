24339a635d333a15ec16a4e53464c1c07fc255a6
bananabread08 <rmkfermin@gmail.com>
2023-03-24 10:12:18 +0800

    finish 6.9
    create filter functionality

--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -1,8 +1,13 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { voteThis } from '../reducers/anecdoteReducer';
+import { filterChange } from '../reducers/filterReducer';
+import { Filter } from './Filter';
 
 export const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector((state) => {
+    if (state.filter === '') return state.anecdotes;
+    else return state.anecdotes.filter((a) => a.content.includes(state.filter));
+  });
   const dispatch = useDispatch();
   const sorted = anecdotes.sort((a, b) => b.votes - a.votes);
 
@@ -10,8 +15,13 @@ export const AnecdoteList = () => {
     dispatch(voteThis(id));
   };
 
+  const filterAnecdotes = (str) => {
+    dispatch(filterChange(str));
+  };
+
   return (
     <>
+      <Filter filterAnecdotes={filterAnecdotes} />
       {sorted.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
--- redux-anecdotes/src/index.js
+++ redux-anecdotes/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './App'
-import reducer from './reducers/anecdoteReducer'
-
-const store = createStore(reducer)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import App from './App';
+import { anecdoteReducer } from './reducers/anecdoteReducer';
+import { filterReducer } from './reducers/filterReducer';
+const reducer = combineReducers({
+  anecdotes: anecdoteReducer,
+  filter: filterReducer,
+});
+const store = createStore(reducer);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>
-)
+);
