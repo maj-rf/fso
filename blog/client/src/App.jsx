bb30e3f01777c4a720df909ceda9da508ab7e9e2
bananabread08 <rmkfermin@gmail.com>
2023-04-15 10:33:52 +0800

    7.16-7.17: create view for single blog post and implement navigation

--- blog/client/src/App.jsx
+++ blog/client/src/App.jsx
@@ -6,15 +6,24 @@ import { User } from './pages/User';
 import { Navbar } from './components/Navbar';
 import { UserContext } from './context/UserContext';
 import { useContext, useEffect } from 'react';
-import { setToken } from './services/blogs';
-import { useQuery } from 'react-query';
+import { getAll, setToken, deleteBlog, updateBlog } from './services/blogs';
+import { useQuery, useQueryClient, useMutation } from 'react-query';
 import { getUsers } from './services/users';
-
+import { Blog } from './components/Blog';
+import { Notification } from './components/Notification';
+import { useNotifDispatch } from './context/NotificationContext';
 function App() {
   const [user, userDispatch] = useContext(UserContext);
   const result = useQuery(['users'], getUsers);
+  const blogResult = useQuery(['blogs'], getAll);
   const match = useMatch('/users/:id');
-  //console.log(result.data);
+  const blogMatch = useMatch('/blogs/:id');
+  const currentBlog = blogResult?.data?.find(
+    (a) => a.id === blogMatch?.params.id,
+  );
+  const notifDispatch = useNotifDispatch();
+  const queryClient = useQueryClient();
+
   useEffect(() => {
     const blogUser = window.localStorage.getItem('blogUser');
     if (blogUser) {
@@ -24,9 +33,44 @@ function App() {
     }
   }, []);
 
+  const setNotification = (payload) => {
+    notifDispatch({
+      type: 'show',
+      payload,
+    });
+    setTimeout(() => notifDispatch({ type: 'hide' }), 3000);
+  };
+
+  const deleteBlogMutation = useMutation(deleteBlog, {
+    onSuccess: () => queryClient.invalidateQueries(['blogs']),
+  });
+
+  const updateBlogMutation = useMutation(updateBlog, {
+    onSuccess: () => queryClient.invalidateQueries(['blogs']),
+  });
+
+  const handleDelete = (blog) => {
+    if (confirm(`Do you want to delete ${blog.title}?`)) {
+      deleteBlogMutation.mutate(blog.id);
+      setNotification({
+        notif: 'Successfully deleted',
+        notifType: 'success',
+      });
+    }
+  };
+
+  const handleLike = (blog) => {
+    updateBlogMutation.mutate({ ...blog, likes: blog.likes + 1 });
+    setNotification({
+      notif: `You liked ${blog.title} by ${blog.author}`,
+      notifType: 'success',
+    });
+  };
+
   return (
     <>
       {user && <Navbar />}
+      <Notification />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/users" element={<Users />} />
@@ -34,6 +78,16 @@ function App() {
           path="/users/:id"
           element={
             <User user={result?.data?.find((a) => a.id === match?.params.id)} />
+          }
+        />
+        <Route
+          path="/blogs/:id"
+          element={
+            <Blog
+              handleDelete={handleDelete}
+              handleLike={handleLike}
+              blog={currentBlog}
+            />
           }
         />
       </Routes>
